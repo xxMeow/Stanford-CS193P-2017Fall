8b5f2bb02129865f1ff36cbdb1ae0bd35108425d
xxMeow <xmx1025@gmail.com>
2019-08-16 18:36:36 +0900

    Implemented Multitouch

---
+++ PlayingCard/PlayingCard/PlayingCardDeck.swift
@@ -0,0 +1,41 @@
+//
+//  PlayingCardDeck.swift
+//  PlayingCard
+//
+//  Created by XMX on 2019/08/8.
+//  Copyright © 2019 XMX. All rights reserved.
+//
+
+import Foundation
+
+struct PlayingCardDeck {
+    private(set) var cards = [PlayingCard]()
+    
+    init() {
+        for suit in PlayingCard.Suit.all {
+            for rank in PlayingCard.Rank.all {
+                cards.append(PlayingCard(suit: suit, rank: rank))
+            }
+        }
+    }
+    
+    mutating func draw() -> PlayingCard? {
+        if cards.count > 0 {
+            return cards.remove(at: cards.count.arc4random)
+        } else {
+            return nil
+        }
+    }
+}
+
+extension Int {
+    var arc4random: Int {
+        if self > 0 {
+            return Int(arc4random_uniform(UInt32(self)))
+        } else if self < 0 {
+            return -Int(arc4random_uniform(UInt32(abs(self))))
+        } else {
+            return 0
+        }
+    }
+}
---
+++ PlayingCard/PlayingCard/ViewController.swift
@@ -0,0 +1,49 @@
+//
+//  ViewController.swift
+//  PlayingCard
+//
+//  Created by XMX on 2019/08/7.
+//  Copyright © 2019 XMX. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    var deck = PlayingCardDeck()
+    
+    @IBOutlet weak var playingCardView: PlayingCardView! {
+        didSet {
+            let swipe = UISwipeGestureRecognizer(target: self, action: #selector(nextCard))
+            swipe.direction = [.left,.right]
+            playingCardView.addGestureRecognizer(swipe)
+            
+            let pinch = UIPinchGestureRecognizer(target: playingCardView, action: #selector(PlayingCardView.adjustFaceCardScale(byHandlingGestureRecognizedBy:)))
+            playingCardView.addGestureRecognizer(pinch)
+        }
+    }
+    
+    @IBAction func flipCard(_ sender: UITapGestureRecognizer) {
+        switch sender.state {
+        case .ended:
+            playingCardView.isFaceUp = !playingCardView.isFaceUp
+        default:
+            break
+        }
+    }
+    
+    @objc func nextCard() {
+        if let card = deck.draw() {
+            playingCardView.rank = card.rank.order
+            playingCardView.suit = card.suit.rawValue
+        }
+    }
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+
+}
+
