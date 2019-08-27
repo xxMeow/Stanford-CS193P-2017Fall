4df3aee8376bbe2332bedef4517abe83308de90b
xxMeow <xmx1025@gmail.com>
2019-08-27 09:37:51 +0900

    Added scroll view from code

--- Cassini/Cassini/ImageViewController.swift
+++ Cassini/Cassini/ImageViewController.swift
@@ -12,10 +12,23 @@ class ImageViewController: UIViewController {
     
     var imageURL: URL? {
         didSet {
-            imageView.image = nil
+            image = nil
             if view.window != nil { // If a view is now on screen, it will get a window
                 fetchImage()
             }
+        }
+    }
+    
+    private var image: UIImage? {
+        get {
+            return imageView.image
+        }
+        set {
+            imageView.image = newValue
+            // Every time set the imageView, resize it
+            imageView.sizeToFit()
+            // And then resize the content size to be equal to the imageView
+            scrollView.contentSize = imageView.frame.size
         }
     }
     
@@ -26,13 +39,19 @@ class ImageViewController: UIViewController {
         }
     }
     
-    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var scrollView: UIScrollView! {
+        didSet {
+            scrollView.addSubview(imageView)
+        }
+    }
+    
+    var imageView = UIImageView()
     
     private func fetchImage() {
         if let url = imageURL {
             let urlContents = try? Data(contentsOf: url)
             if let imageData = urlContents {
-                imageView.image = UIImage(data: imageData)
+                 image = UIImage(data: imageData)
             }
         }
     }
